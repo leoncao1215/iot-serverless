c63e3ff564caac14dbf8c53e7ab680912b230332
leoncao1215 <929509707@qq.com>
2020-03-30 21:01:05 +0800

    获取所有类型 & 查询某一类型的所有设备

--- src/app/device/Device.js
+++ src/app/device/Device.js
@@ -1,89 +1,209 @@
 import request from "../../util/request-util";
 import React from "react";
-import {Table, Menu, Icon, Button, Header, Segment} from "semantic-ui-react";
+import {Table, Menu, Icon, Button, Header, Segment, Select, Dropdown} from "semantic-ui-react";
 
 export default class Device extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      devices: []
+      devices  : [],
+      types    : [],
+      action   : '',
+      curDevice: {}
     }
   }
 
   componentDidMount() {
-    request.get('/queryDeviceList')
-      .then(res =>
-        this.setState({
-          devices: res.devices
-        })
-      );
-  }
-
-  getDeviceStatus = (device) => {
-    if (device.disabled) {
-      return 'Disabled';
-    } else if (device.down) {
-      return 'Offline';
-    } else {
-      return 'Normal';
-    }
-  };
-
-  get deviceRows() {
-    return this.state.devices.map(device =>
-      <Table.Row>
-        <Table.Cell>{device.serialNumber}</Table.Cell>
-        <Table.Cell>{device.deviceName}</Table.Cell>
-        <Table.Cell>{this.getDeviceStatus(device)}</Table.Cell>
-        <Table.Cell collapsing>
-          <Button.Group>
-            <Button positive>View</Button>
-            <Button.Or />
-            <Button>Edit</Button>
-          </Button.Group>
-        </Table.Cell>
-      </Table.Row>
-    )
+    this.fetchDevices();
+    this.fetchTypes();
   }
 
   render() {
     return (
-      <div className="device-list">
-        {/*<Segment clearing>*/}
-          <Header as='h1' floated='left'>Devices List</Header>
-          <Button basic floated='right' color='green'>Add Device</Button>
-        {/*</Segment>*/}
-        <Table celled>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Serial Number</Table.HeaderCell>
-              <Table.HeaderCell>Device Name</Table.HeaderCell>
-              <Table.HeaderCell>Status</Table.HeaderCell>
-              <Table.HeaderCell>Actions</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-
-          <Table.Body>
-            {this.deviceRows}
-          </Table.Body>
-
-          <Table.Footer>
-            <Table.Row>
-              <Table.HeaderCell colSpan='4'>
-                <Menu floated='right' pagination>
-                  <Menu.Item as='a' icon>
-                    <Icon name='chevron left' />
-                  </Menu.Item>
-                  <Menu.Item as='a'>1</Menu.Item>
-                  <Menu.Item as='a' icon>
-                    <Icon name='chevron right' />
-                  </Menu.Item>
-                </Menu>
-              </Table.HeaderCell>
-            </Table.Row>
-          </Table.Footer>
-        </Table>
+      <div>
+        {this.deviceTable}
+        {this.actionPane}
       </div>
     )
+  };
+
+  fetchDevices = (type) => {
+    if (!type) {
+      request.get('/queryDeviceList')
+        .then(res =>
+          this.setState({
+            devices: res.devices
+          })
+        );
+    } else {
+      request.get(`/queryDeviceListByType?type=${type}`)
+        .then(res =>
+          this.setState({
+            devices: res.devices
+          })
+        )
+    }
+  };
+
+  fetchTypes = () => {
+    request.get('/queryDeviceTypeList')
+      .then(res =>
+        this.setState({
+          types: res.types
+        })
+      );
+  };
+
+  get deviceTable() {
+    const options = [
+      {key: 'none', value: 'none', text: '-- none --'},
+    ];
+
+    this.state.types.forEach(type => options.push({key: type, value: type, text: type}));
+
+    const getDeviceStatus = (device) => {
+      if (device.disabled) {
+        return 'Disabled';
+      } else if (device.down) {
+        return 'Offline';
+      } else {
+        return 'Normal';
+      }
+    };
+
+    return (
+      <div>
+        <div className="device-list">
+          <Header as='h1' floated='left'>Devices List</Header>
+          <Button basic floated='right' color='green' onClick={this.handleAddDevice}>Add Device</Button>
+          <Dropdown search selection onChange={this.handleChangeType}
+                    placeholder='Filter device type' options={options}/>
+          <Button basic onClick={this.handleFilter} style={{marginLeft: '.5em'}}>Filter</Button>
+          <Table celled>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Serial Number</Table.HeaderCell>
+                <Table.HeaderCell>Type</Table.HeaderCell>
+                <Table.HeaderCell>Device Name</Table.HeaderCell>
+                <Table.HeaderCell>Status</Table.HeaderCell>
+                <Table.HeaderCell>Actions</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+              {this.state.devices.map(device =>
+                <Table.Row>
+                  <Table.Cell collapsing>{device.serialNumber}</Table.Cell>
+                  <Table.Cell collapsing>{device.type}</Table.Cell>
+                  <Table.Cell>{device.deviceName}</Table.Cell>
+                  <Table.Cell collapsing>{getDeviceStatus(device)}</Table.Cell>
+                  <Table.Cell collapsing>
+                    <Button.Group>
+                      <Button positive onClick={() => this.handleViewDevice(device)}>View</Button>
+                      <Button.Or/>
+                      <Button onClick={() => this.handleEditDevice(device)}>Edit</Button>
+                    </Button.Group>
+                  </Table.Cell>
+                </Table.Row>
+              )}
+            </Table.Body>
+
+            <Table.Footer>
+              <Table.Row>
+                <Table.HeaderCell colSpan='5'>
+                  <Menu floated='right' pagination>
+                    <Menu.Item as='a' icon>
+                      <Icon name='chevron left'/>
+                    </Menu.Item>
+                    <Menu.Item as='a'>1</Menu.Item>
+                    <Menu.Item as='a' icon>
+                      <Icon name='chevron right'/>
+                    </Menu.Item>
+                  </Menu>
+                </Table.HeaderCell>
+              </Table.Row>
+            </Table.Footer>
+          </Table>
+        </div>
+      </div>
+    )
+  }
+
+  get actionPane() {
+    const {curDevice} = this.state;
+    switch (this.state.action) {
+      case 'add':
+        return (
+          <Segment>
+            <Header as='h1'>Add Pane</Header>
+            <Button onClick={this.handleCancel}>Cancel</Button>
+          </Segment>
+        );
+      case 'view':
+        return (
+          <Segment>
+            <Header as='h1'>Device: {`${curDevice.deviceName} (${curDevice.serialNumber})`}</Header>
+            <Button onClick={() => this.handleEditDevice()}>Edit</Button>
+          </Segment>
+        );
+      case 'edit':
+        return (
+          <Segment>
+            <Header as='h1'>Editing Device: {`${curDevice.deviceName} (${curDevice.serialNumber})`}</Header>
+            <Button onClick={() => this.handleViewDevice()}>Cancel</Button>
+          </Segment>
+        );
+      default:
+        return undefined;
+    }
+  }
+
+  handleChangeType = (e, data) => {
+    this.setState({
+      type: data.value
+    })
+  };
+
+  handleFilter = () => {
+    const {type} = this.state;
+    this.fetchDevices(type === 'none' ? undefined : type);
+  };
+
+  handleAddDevice = () => {
+    this.setState({
+      action: 'add'
+    })
+  };
+
+  handleViewDevice = (device) => {
+    if (device) {
+      this.setState({
+        action   : 'view',
+        curDevice: device
+      })
+    } else {
+      this.setState({
+        action: 'view',
+      })
+    }
+  };
+
+  handleEditDevice = (device) => {
+    if (device) {
+      this.setState({
+        action   : 'edit',
+        curDevice: device
+      })
+    } else {
+      this.setState({
+        action: 'edit',
+      })
+    }
+  };
+
+  handleCancel = () => {
+    this.setState({
+      action: ''
+    })
   };
 }
--- srv/deviceHandler.js
+++ srv/deviceHandler.js
@@ -2,7 +2,13 @@
 
 const util = require('./util');
 
-// 添加单个设备
+/**
+ * 添加单个设备
+ *
+ * @param event
+ * @param context
+ * @param callback
+ */
 module.exports.addDevice = (event, context, callback) => {
   const dynamodb = require('./dynamodb');
 
@@ -40,7 +46,13 @@ module.exports.addDevice = (event, context, callback) => {
   })
 };
 
-// 查询设备列表
+/**
+ * 查询所有设备
+ *
+ * @param event
+ * @param context
+ * @param callback
+ */
 module.exports.queryDeviceList = (event, context, callback) => {
   const dynamodb = require('./dynamodb');
 
@@ -53,6 +65,60 @@ module.exports.queryDeviceList = (event, context, callback) => {
     } else if ("Items" in res) {
       response.statusCode = 200;
       response.body       = JSON.stringify({devices: res["Items"]});
+    }
+    callback(null, response);
+  });
+};
+
+/**
+ * 查询所有设备类别
+ *
+ * @param event
+ * @param context
+ * @param callback
+ */
+module.exports.queryDeviceTypeList = (event, context, callback) => {
+  const dynamodb = require('./dynamodb');
+
+  dynamodb.scan({TableName: 'device'}, (err, res) => {
+    const response = {statusCode: null, body: null};
+    if (err) {
+      console.log(err);
+      response.statusCode = 500;
+      response.body       = JSON.stringify({code: 500, message: "ScanItem Error"});
+    } else if ("Items" in res) {
+      const types = new Set();
+      res['Items'].forEach(device => types.add(device.type));
+      response.statusCode = 200;
+      response.body       = JSON.stringify({types: Array.from(types)});
+    }
+    callback(null, response);
+  });
+};
+
+/**
+ * 查询所有某一类型的设备
+ *
+ * @param event
+ * @param context
+ * @param callback
+ */
+module.exports.queryDeviceListByType = (event, context, callback) => {
+  const dynamodb = require('./dynamodb');
+
+  const type = event.queryStringParameters.type;
+
+  dynamodb.scan({TableName: 'device'}, (err, res) => {
+    const response = {statusCode: null, body: null};
+    if (err) {
+      console.log(err);
+      response.statusCode = 500;
+      response.body       = JSON.stringify({code: 500, message: "ScanItem Error"});
+    } else if ("Items" in res) {
+      const devices = [];
+      res['Items'].forEach(device => {if (device.type === type) devices.push(device)});
+      response.statusCode = 200;
+      response.body       = JSON.stringify({devices: devices});
     }
     callback(null, response);
   });
